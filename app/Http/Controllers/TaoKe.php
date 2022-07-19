b9d5a17d72125d9b9d84cf9c76de3d4b386419a4
tangxuning <tangxuning@huoban.com>
2022-07-19 19:29:14 +0800

    222

--- app/Http/Controllers/TaoKe.php
+++ app/Http/Controllers/TaoKe.php
@@ -171,7 +171,7 @@ class TaoKe extends Controller
         }elseif($kind == 'tb'){
 
             $short_url = $this->TbkDgMaterialOptional($source_url);
-            if(!isset($short_url['long_url'])  and !empty($short_url['long_url']))
+            if(isset($short_url['long_url'])  and !empty($short_url['long_url']))
                 $result = ['data'=>['url'=>$short_url['long_url'],'coupon_share_url'=>$short_url['coupon_share_url']]];
             else
                 $result = ['data'=>['url'=>$short_url,'coupon_share_url'=>'没有优惠券']];
